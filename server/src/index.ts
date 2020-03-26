d31684d07cbe438e69cfe9a9ab2d73c1ad3b7711
Dan Gurney <gurney.dan@gmail.com>
2020-03-26 20:35:28 +0000

    Set up basic Apollo Server Express boilerplate

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import depthLimit from 'graphql-depth-limit';
+import { createServer } from 'http';
+import compression from 'compression';
+import cors from 'cors';
+import 'reflect-metadata';
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  validationRules: [depthLimit(7)]
+});
+
+const app = express();
+app.use('*', cors());
+app.use(compression());
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
