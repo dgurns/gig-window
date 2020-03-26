e856bbca37c0b8004ae287a936e30d12ac8b15fb
Dan Gurney <gurney.dan@gmail.com>
2020-03-26 21:15:50 +0000

    Fix paths issue in tsconfig

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,40 @@
+import 'reflect-metadata';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import depthLimit from 'graphql-depth-limit';
-import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
-import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import { createConnection } from 'typeorm';
+import { buildSchema } from 'type-graphql';
+import depthLimit from 'graphql-depth-limit';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
+import { BookResolver } from 'resolvers/BookResolver';
+
+async function start() {
+  try {
+    const connection = await createConnection();
+    const schema = await buildSchema({
+      resolvers: [BookResolver]
+    });
+
+    const server = new ApolloServer({
+      schema,
+      validationRules: [depthLimit(7)]
+    });
+
+    const app = express();
+    app.use('*', cors());
+    app.use(compression());
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () =>
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      )
+    );
+  } catch (error) {
+    console.log(error);
   }
-`;
+}
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!'
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  validationRules: [depthLimit(7)]
-});
-
-const app = express();
-app.use('*', cors());
-app.use(compression());
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+start();
