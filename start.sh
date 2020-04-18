fbe9a09e2a00a3cf3901d3428abbff062f8cffb3
Dan Gurney <gurney.dan@gmail.com>
2020-04-18 16:09:15 +0100

    Don't expose local api with ngrok, since Docker container can use host.docker.internal

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Start web
-# Start api and make it publicly accessible
+# Start api
 # Start rtmp and make it publicly accessible
 
-# NOTE: You'll need to replace these ngrok domains 
-# with ones from your own ngrok account
+# NOTE: You'll need to replace the ngrok remote address
+# with one from your own ngrok account
 
 concurrently \
   "cd web && yarn start" \
-  "ngrok http -subdomain=corona-window-api 4000" \
   "cd api && yarn start" \
   "ngrok tcp --remote-addr 1.tcp.ngrok.io:29644 1935" \
-  "docker-compose -f rtmp/docker-compose.yml up"
+  "docker-compose -f rtmp/docker-compose.yml up --build"
