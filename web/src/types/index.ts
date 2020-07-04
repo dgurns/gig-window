676bce4a4257a054d5494f1f8479f95201e099e1
Dan Gurney <gurney.dan@gmail.com>
2020-07-04 17:34:00 +0100

    Duplicate types in web folder. Add Dockerfile for web.

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -1,9 +1,59 @@
-import { User } from '../../../api/src/entities/User';
-import { Chat } from '../../../api/src/entities/Chat';
-import { Payment } from '../../../api/src/entities/Payment';
-import { ChatEvent } from '../../../api/src/entities/ChatEvent';
+export interface User {
+  id: number;
+  email: string;
+  username: string;
+  urlSlug: string;
+  hashedPassword: string;
+  streamKey: string;
+  isPublishingStream: boolean;
+  isInPublicMode: boolean;
+  lastPublishedStreamStartTimestamp: string;
+  lastPublishedStreamEndTimestamp: string;
+  liveVideoInfrastructureError: string;
+  awsMediaLiveInputId: string;
+  awsMediaLiveChannelId: string;
+  awsMediaPackageChannelId: string;
+  awsMediaPackageChannelIngestUrl: string;
+  awsMediaPackageChannelIngestUsername: string;
+  awsMediaPackageChannelIngestPasswordParam: string;
+  awsMediaPackageOriginEndpointId: string;
+  awsMediaPackageOriginEndpointUrl: string;
+  profileImageUrl: string;
+  stripeCustomerId: string;
+  stripeAccountId: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
-export { User, Chat, Payment, ChatEvent };
+export interface Chat {
+  id: number;
+  user: User;
+  userId: number;
+  parentUser: User;
+  parentUserId: number;
+  message: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Payment {
+  id: number;
+  amountInCents: number;
+  stripePaymentIntentId: string;
+  user: User;
+  userId: number;
+  payeeUser: User;
+  payeeUserId: number;
+  showId?: number;
+  isRefunded?: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ChatEvent {
+  chat?: Chat;
+  payment?: Payment;
+}
 
 export interface Show {
   id: number;
