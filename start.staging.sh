17ef3b0b56125003b34e31ab438feedd293f72d0
Dan Gurney <gurney.dan@gmail.com>
2020-08-07 17:10:46 +0100

    Update start.staging script to pull latest Docker images

--- start.staging.sh
+++ start.staging.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script starts the full backend (nginx, api, 
-# redis, and rtmp) and provides it with staging env 
-# variables. It also removes any outdated Docker 
-# images.
+# This script pulls updated Docker images and then
+# starts the full backend (nginx, api, redis, and 
+# rtmp), providing it with staging env variables. 
+# It also removes any outdated Docker images.
 
 # This script is meant to be run on the server where 
 # you are deploying the staging environment.
@@ -11,5 +11,10 @@
 # The staging web app is served separately via Vercel
 # but points to this backend.
 
+docker pull dgurney/corona-window-nginx:latest
+docker pull dgurney/corona-window-api:latest
+docker pull dgurney/corona-window-rtmp:latest
+
 docker-compose -f docker-compose.staging.yml up --build -d
+
 docker image prune -f
