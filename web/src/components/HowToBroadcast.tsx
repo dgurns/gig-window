e5720d9cd3a85c4c462a385508537009b56f97cc
Dan Gurney <gurney.dan@gmail.com>
2020-08-08 18:00:32 +0100

    Add quickstart info for OBS and Streamlabs

--- web/src/components/HowToBroadcast.tsx
+++ web/src/components/HowToBroadcast.tsx
@@ -32,6 +32,30 @@ const HowToBroadcast = () => {
         of help resources and guides for getting started.
       </Typography>
       <Typography className={classes.howToItem}>
+        <em>Streamlabs quickstart:</em>
+        <br />
+        - Download app
+        <br />
+        - On login screen tap "Other Platforms"
+        <br />
+        - Copy/paste RTMP URL and stream key from below
+        <br />
+        - Tap "Save" and close Settings screen
+        <br />- Press the red button to start streaming
+      </Typography>
+      <Typography className={classes.howToItem}>
+        <em>OBS quickstart:</em>
+        <br />
+        - Download software
+        <br />
+        - Click "Settings" in bottom right and go to the "Stream" tab
+        <br />
+        - Choose "Service: Custom...", copy/paste the RTMP URL from below into
+        the "Server" field, and copy/paste the Stream Key
+        <br />- Click "Apply", then "OK" and close the window
+        <br />- Click "Start Streaming"
+      </Typography>
+      <Typography className={classes.howToItem}>
         2. Send your stream to this RTMP URL:
       </Typography>
       <TextField
