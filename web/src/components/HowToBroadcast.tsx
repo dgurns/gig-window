b180c9006eae40179b49f9e11ae2eb1abfa3de25
Dan Gurney <gurney.dan@gmail.com>
2020-08-23 15:40:19 +0100

    Add expand buttons to quickstarts

--- web/src/components/HowToBroadcast.tsx
+++ web/src/components/HowToBroadcast.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Link, Typography, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import classnames from 'classnames';
 
 import useCurrentUser from 'hooks/useCurrentUser';
 
+import TextButton from 'components/TextButton';
 import CopyToClipboardButton from 'components/CopyToClipboardButton';
 
-const useStyles = makeStyles(({ spacing }) => ({
-  title: {
-    marginBottom: spacing(2),
-  },
+const useStyles = makeStyles(({ spacing, palette }) => ({
   section: {
-    marginBottom: spacing(2),
+    marginTop: spacing(2),
   },
   item: {
-    marginBottom: spacing(2),
+    marginTop: spacing(2),
+  },
+  quickstartContainer: {
+    marginLeft: spacing(2),
+    marginTop: spacing(2),
+  },
+  quickstartToggle: {
+    marginLeft: spacing(1),
+    paddingBottom: 3,
+  },
+  quickstartBody: {
+    borderLeft: `1px solid ${palette.secondary.main}`,
+    marginLeft: spacing(2),
+    paddingLeft: spacing(2),
   },
   rtmpField: {
     maxWidth: 370,
@@ -32,13 +43,17 @@ const HowToBroadcast = () => {
   const classes = useStyles();
   const [currentUser] = useCurrentUser();
 
+  const [
+    streamlabsQuickstartIsExpanded,
+    setStreamlabsQuickstartIsExpanded,
+  ] = useState(false);
+  const [obsQuickstartIsExpanded, setObsQuickstartIsExpanded] = useState(false);
+
   return (
     <Grid container direction="column">
-      <Typography variant="h6" className={classes.title}>
-        How to broadcast:
-      </Typography>
+      <Typography variant="h6">How to broadcast:</Typography>
       <Grid className={classes.section}>
-        <Typography className={classes.item}>
+        <Typography>
           1. Pick a streaming app - anything that lets you stream to an RTMP
           URL. Many people like <Link href="https://obsproject.com/">OBS</Link>{' '}
           (laptop/desktop) or{' '}
@@ -46,35 +61,54 @@ const HowToBroadcast = () => {
           (iPad/iPhone/Android), which are free and open source. Both have
           plenty of help resources and guides for getting started.
         </Typography>
-        <Typography className={classes.item}>
-          <em>Streamlabs quickstart:</em>
-          <br />
-          - Download app
-          <br />
-          - On login screen tap "Other Platforms"
-          <br />
-          - Copy/paste RTMP URL and stream key from below
-          <br />
-          - Ignore the part about selecting widgets, just click "Next"
-          <br />- Press the red button to start streaming
+        <Typography className={classes.quickstartContainer}>
+          <em>Streamlabs quickstart</em>
+          <TextButton
+            onClick={() =>
+              setStreamlabsQuickstartIsExpanded(!streamlabsQuickstartIsExpanded)
+            }
+            className={classes.quickstartToggle}
+          >
+            {streamlabsQuickstartIsExpanded ? 'Minimize' : 'Expand'}
+          </TextButton>
         </Typography>
-        <Typography className={classes.item}>
-          <em>OBS quickstart:</em>
-          <br />
-          - Download OBS
-          <br />
-          - In OBS, click "Settings" in bottom right and go to the "Stream" tab
-          <br />
-          - Choose "Service: Custom...", copy/paste the RTMP URL from below into
-          the "Server" field, and copy/paste the Stream Key
-          <br />- Click "Apply", then "OK" and close the window
-          <br />- Click "Start Streaming"
+        {streamlabsQuickstartIsExpanded && (
+          <Typography className={classes.quickstartBody}>
+            - Download app
+            <br />
+            - On login screen tap "Other Platforms"
+            <br />
+            - Copy/paste RTMP URL and stream key from below
+            <br />
+            - Ignore the part about selecting widgets, just click "Next"
+            <br />- Press the red button to start streaming
+          </Typography>
+        )}
+        <Typography className={classes.quickstartContainer}>
+          <em>OBS quickstart</em>
+          <TextButton
+            onClick={() => setObsQuickstartIsExpanded(!obsQuickstartIsExpanded)}
+            className={classes.quickstartToggle}
+          >
+            {obsQuickstartIsExpanded ? 'Minimize' : 'Expand'}
+          </TextButton>
         </Typography>
+        {obsQuickstartIsExpanded && (
+          <Typography className={classes.quickstartBody}>
+            - Download OBS
+            <br />
+            - In OBS, click "Settings" in bottom right and go to the "Stream"
+            tab
+            <br />
+            - Choose "Service: Custom...", copy/paste the RTMP URL from below
+            into the "Server" field, and copy/paste the Stream Key
+            <br />- Click "Apply", then "OK" and close the window
+            <br />- Click "Start Streaming"
+          </Typography>
+        )}
       </Grid>
       <Grid className={classes.section}>
-        <Typography className={classes.item}>
-          2. Send your stream to this RTMP URL:
-        </Typography>
+        <Typography>2. Send your stream to this RTMP URL:</Typography>
         <TextField
           value={process.env.REACT_APP_RTMP_URL}
           variant="outlined"
@@ -108,7 +142,7 @@ const HowToBroadcast = () => {
         />
       </Grid>
       <Grid className={classes.section}>
-        <Typography className={classes.item}>
+        <Typography>
           3. When you’re broadcasting, you’ll see the stream appear above.
         </Typography>
       </Grid>
