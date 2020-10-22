735f79ff955d8a3bbcb4d05e6114793a61527144
Dan Gurney <dan@dangurney.net>
2020-10-22 13:39:03 +0100

    [Release]: Emoji picker in chat (#40)
    * Add EmojiPicker
    
    * Make emojis display bigger in chat

---
+++ web/src/components/EmojiPicker.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+export const EMOJI_OPTIONS = ['👍', '😂', '🎉', '👏', '🔥', '💯'];
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  container: {
+    backgroundColor: palette.common.white,
+    borderRadius: spacing(1),
+    fontSize: '1.5rem',
+    lineHeight: '1.5rem',
+    margin: 2,
+    width: 'auto',
+  },
+  trigger: {
+    cursor: 'pointer',
+    filter: 'grayscale(100%)',
+    padding: spacing(1),
+  },
+  emojiOption: {
+    cursor: 'pointer',
+    padding: spacing(1),
+  },
+}));
+
+interface Props {
+  onEmojiPicked: (emoji: string) => void;
+  className?: string;
+}
+
+const EmojiPicker = ({ onEmojiPicked, className }: Props) => {
+  const classes = useStyles();
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const onTriggerClicked = () => setIsExpanded(!isExpanded);
+
+  const onEmojiClicked = (emoji: string) => {
+    setIsExpanded(false);
+    onEmojiPicked(emoji);
+  };
+
+  return (
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      justify="flex-end"
+      className={classnames(classes.container, className)}
+    >
+      {isExpanded &&
+        EMOJI_OPTIONS.map((emoji, index) => (
+          <span
+            onClick={() => onEmojiClicked(emoji)}
+            className={classes.emojiOption}
+            role="img"
+            aria-label={emoji}
+            key={index}
+          >
+            {emoji}
+          </span>
+        ))}
+      <span
+        onClick={onTriggerClicked}
+        className={classes.trigger}
+        role="img"
+        aria-label="Open emoji picker"
+      >
+        {isExpanded ? '🙈' : '😃'}
+      </span>
+    </Grid>
+  );
+};
+
+export default React.memo(EmojiPicker);
