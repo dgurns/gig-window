fa721bca73134cfb287a87f22688604d9cf7b10b
Dan Gurney <gurney.dan@gmail.com>
2020-10-24 18:38:07 +0100

    Update tests

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { render, screen, wait } from "test-utils";
-import App from "App";
+import React from 'react';
+import { render, screen, wait } from 'test-utils';
+import App from 'App';
 
-describe("App component", () => {
-  it("should not display UI while fetching current user status", async () => {
+describe('App component', () => {
+  it('should not display UI while fetching current user status', async () => {
     const { container } = render(<App />);
     expect(container.firstChild).toBe(null);
     await screen.findByText(/Live now/i);
   });
 
-  it("should display UI after the current user has been fetched", async () => {
+  it('should display UI after the current user has been fetched', async () => {
     render(<App />);
 
     await screen.findByText(/Live now/i);
@@ -21,7 +21,7 @@ describe("App component", () => {
     screen.getByText(/Dan's show/i);
     screen.getAllByText(/Github/i, { exact: false });
 
-    expect(screen.queryByText(/Monetize your live streams/i)).toBe(null);
+    expect(screen.queryByText(/Play online shows/i)).toBe(null);
     expect(screen.queryByText(/Log in/i)).toBe(null);
     expect(screen.queryByText(/Sign up/i)).toBe(null);
   });
