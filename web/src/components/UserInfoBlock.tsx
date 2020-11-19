78d4e3bfd3bf791c9118bfd5426f1a114e523f9a
Dan Gurney <gurney.dan@gmail.com>
2020-11-19 15:06:25 +0000

    Add basic implementation for upcoming schedule dialog

--- web/src/components/UserInfoBlock.tsx
+++ web/src/components/UserInfoBlock.tsx
@@ -2,25 +2,42 @@ import React, { useMemo } from 'react';
 import classnames from 'classnames';
 import { Grid, Typography, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import ExpandIcon from '@material-ui/icons/KeyboardArrowDown';
 
 import { User } from 'types';
 import Image from 'services/Image';
 import DateTime from 'services/DateTime';
 import useShowsForUser from 'hooks/useShowsForUser';
+import useDialog from 'hooks/useDialog';
 
-const useStyles = makeStyles(({ spacing }) => ({
-  userInfoContainer: {
+import UpcomingShowsList from 'components/UpcomingShowsList';
+
+const useStyles = makeStyles(({ spacing, palette }) => ({
+  container: {
     alignItems: 'center',
   },
-  userImage: {
+  image: {
     borderRadius: spacing(1),
     height: 80,
     marginRight: spacing(2),
     width: 80 * Image.DEFAULT_IMAGE_ASPECT_RATIO,
   },
-  userText: {
+  textContainer: {
     flex: 1,
     flexDirection: 'column',
+  },
+  description: {
+    color: palette.secondary.main,
+  },
+  activeShowDescription: {
+    color: palette.secondary.main,
+    cursor: 'pointer',
+    '&:hover': {
+      color: palette.secondary.dark,
+    },
+  },
+  expandIcon: {
+    marginBottom: -7,
   },
 }));
 
@@ -32,47 +49,60 @@ interface Props {
 const UserInfoBlock = ({ user, className }: Props) => {
   const classes = useStyles();
 
-  const [, showsQuery, activeShow] = useShowsForUser(user?.id);
+  const [shows, showsQuery, activeShow] = useShowsForUser(user?.id);
+  const [UpcomingShowsDialog, setUpcomingShowsDialogIsVisible] = useDialog();
 
   const activeShowDescription = useMemo(() => {
     if (showsQuery.loading) {
       return <CircularProgress size={19} color="secondary" />;
     } else if (showsQuery.error) {
       return (
-        <Typography color="textSecondary">Error fetching shows</Typography>
+        <Typography className={classes.description}>
+          Error fetching shows
+        </Typography>
       );
     } else if (activeShow) {
       return (
-        <Typography color="textSecondary">
-          {`${DateTime.formatUserReadableShowtime(activeShow.showtime)}: ${
-            activeShow.title
-          }`}
-        </Typography>
+        <Grid container direction="row">
+          <Typography
+            className={classes.activeShowDescription}
+            onClick={() => setUpcomingShowsDialogIsVisible(true)}
+          >
+            {`${DateTime.formatUserReadableShowtime(activeShow.showtime)}: ${
+              activeShow.title
+            }`}
+            <ExpandIcon className={classes.expandIcon} />
+          </Typography>
+        </Grid>
       );
     } else {
-      return <Typography color="textSecondary">No shows scheduled</Typography>;
+      return (
+        <Typography className={classes.description}>
+          No shows scheduled
+        </Typography>
+      );
     }
-  }, [showsQuery, activeShow]);
+  }, [showsQuery, activeShow, classes, setUpcomingShowsDialogIsVisible]);
 
   return (
     <Grid
       container
       direction="row"
-      className={classnames(classes.userInfoContainer, className)}
+      className={classnames(classes.container, className)}
     >
       {user.profileImageUrl && (
-        <img
-          src={user.profileImageUrl}
-          alt="Watch"
-          className={classes.userImage}
-        />
+        <img src={user.profileImageUrl} alt="Watch" className={classes.image} />
       )}
-      <Grid item className={classes.userText}>
+      <Grid item className={classes.textContainer}>
         <Typography variant="h6" color="inherit">
           {user.username}
         </Typography>
         {user?.isAllowedToStream && activeShowDescription}
       </Grid>
+
+      <UpcomingShowsDialog>
+        <UpcomingShowsList shows={shows} />
+      </UpcomingShowsDialog>
     </Grid>
   );
 };
