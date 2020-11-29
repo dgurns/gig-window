06890f580b7646570f1aff60bdd3296f9f73f313
Dan Gurney <dan@dangurney.net>
2020-11-29 16:41:32 +0000

    Remove cors

--- api/src/middlewares/authenticateMuxWebhookRequest.ts
+++ api/src/middlewares/authenticateMuxWebhookRequest.ts
@@ -1,35 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
 
 import Mux from 'services/Mux';
-import { SERVER_PORT } from '../';
-
-const { WEB_ORIGIN, NODE_ENV } = process.env;
-
-// const applyCorsToRequest = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const allowedOrigins = [WEB_ORIGIN];
-//   if (NODE_ENV === 'development') {
-//     allowedOrigins.push(`http://localhost:${SERVER_PORT}`);
-//   }
-//   const corsHandler = cors({
-//     origin: function (origin, callback) {
-//       console.log('origin', origin);
-//       if (allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Request blocked by CORS'));
-//       }
-//     },
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   });
-
-//   corsHandler(req, res, next);
-// };
 
 export const authenticateMuxWebhookRequest = (
   req: Request,
