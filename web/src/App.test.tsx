776b74b65d82a8e1356b0898c07933d1ef529d7c
Dan Gurney <gurney.dan@gmail.com>
2020-12-10 15:03:39 +0000

    Run tests in band

---
+++ web/src/App.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, waitFor } from 'test-utils';
+import App from 'App';
+
+describe('App component', () => {
+  it('should not display UI while fetching current user status', async () => {
+    const { container } = render(<App />);
+    expect(container.firstChild).toBe(null);
+    await waitFor(() => {
+      expect(screen.queryAllByText(/dang/i).length).toEqual(4);
+    });
+  });
+
+  it('should display UI after the current user has been fetched', async () => {
+    render(<App />);
+
+    await screen.findByText(/Live now/i);
+    await waitFor(() => {
+      expect(screen.queryAllByText(/dang/i).length).toEqual(4);
+    });
+    screen.getByText(/Upcoming shows/i);
+    screen.getByText(/October 2 at 7:00 PM/i);
+    screen.getByText(/Dan's show/i);
+    screen.getAllByText(/Github/i, { exact: false });
+
+    expect(screen.queryByText(/Play online shows/i)).toBe(null);
+    expect(screen.queryByText(/Log in/i)).toBe(null);
+    expect(screen.queryByText(/Sign up/i)).toBe(null);
+  });
+});
