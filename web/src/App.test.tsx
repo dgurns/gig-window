6a82bfa494e69b70e20f463e6c093daa8c1cf01d
Dan Gurney <dan@dangurney.net>
2020-12-10 15:49:15 +0000

    [Chore]: Update packages (#88)
    * Remove test that was not working
    
    * Fix typescript error
    
    * Run tests in band
    
    * Downgrade msw
    
    * Try another version of msw

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { render, screen, wait } from 'test-utils';
+import { render, screen, waitFor } from 'test-utils';
 import App from 'App';
 
 describe('App component', () => {
   it('should not display UI while fetching current user status', async () => {
     const { container } = render(<App />);
     expect(container.firstChild).toBe(null);
+
     await screen.findByText(/Live now/i);
+    await waitFor(() => {
+      expect(screen.queryAllByText(/dang/i).length).toEqual(4);
+    });
   });
 
   it('should display UI after the current user has been fetched', async () => {
     render(<App />);
 
     await screen.findByText(/Live now/i);
-    await wait(() => {
+    await waitFor(() => {
       expect(screen.queryAllByText(/dang/i).length).toEqual(4);
     });
     screen.getByText(/Upcoming shows/i);
