61f9a818346a63a0059e360cecd93b8e0171a29a
Dan Gurney <gurney.dan@gmail.com>
2021-06-10 11:53:06 +0100

    Update homepage to reflect being archived

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
-import AppProviders from "./AppProviders";
-import App from "App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import AppProviders from './AppProviders';
+import App from 'App';
 
 ReactDOM.render(
   <AppProviders>
     <App />
   </AppProviders>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
